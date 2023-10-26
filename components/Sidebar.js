b5962a2c53678b4881f0212a8b3b8a90db6896f8
victor <victor.h@clarusway.com>
2023-10-26 19:47:30 +0200

    added tw logo

---
+++ components/Sidebar.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+ 
+export default function Sidebar() {
+  return (
+    <div>
+      
+      {/* Twitter Logo */}
+      <div>
+        <Image
+        width={50}
+        height={50} 
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/220px-Logo_of_Twitter.svg.png">
+
+        </Image>
+      </div>
+      {/* Menu */}
+      {/* Button */}
+      {/* Mini Profie */}
+    </div>
+  )
+}
