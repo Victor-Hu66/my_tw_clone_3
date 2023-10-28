a01cda82db0c914808fef45def21c430b6a7152c
victor <victor.h@clarusway.com>
2023-10-28 12:53:42 +0200

    add sidebar tweet button  style

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -13,10 +13,10 @@ import {
 
 export default function Sidebar() {
   return (
-    <div className="">
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
 
       {/* Twitter Logo */}
-      <div className="hoverEffect">
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
         <Image
           width={50}
           height={50}
@@ -25,7 +25,7 @@ export default function Sidebar() {
       </div>
 
       {/* Menu */}
-      <div>
+      <div className="mt-4 mb-2.5 xl:items-start">
         <SidebarMenuItem text="Home" Icon={HomeIcon} active/>
         <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
         <SidebarMenuItem text="Notifications" Icon={BellIcon} />
@@ -40,17 +40,17 @@ export default function Sidebar() {
       <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-sm hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
       
       {/* Mini Profie */}
-      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+      <div className="hoverEffect  text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
         <img
-          className="rounded-full"
+          className="rounded-full h-10 w-10 xl:mr-2"
           src="https://render.fineartamerica.com/images/rendered/small/flat/round-beach-towel/images-medium-5/fatih-sultan-mehmed-ii-gentile-bellini.jpg?transparent=0&targetx=0&targety=-137&imagewidth=788&imageheight=1063&modelwidth=788&modelheight=788&backgroundcolor=2D181D&orientation=0&producttype=beachtowelround"
           alt="user-img"
         />
-        <div>
-          <h4>Fatih Sultan Mehmet"</h4>
-          <p>@FatihTheConqurer</p>
+        <div className="leading-5 hidden xl:inline">
+          <h4 className="font-bold">Fatih Sultan Mehmet</h4>
+          <p className="text-gray-500">@FatihTheConqurer</p>
         </div>
-        <DotsHorizontalIcon className="h-5" />
+        <DotsHorizontalIcon className="h-5 xl:ml-8" />
       </div>
     </div>
   );
