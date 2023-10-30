d306ee75579fc6f85a86a2fc3740bba47e11e96c
victor <victor.h@clarusway.com>
2023-10-30 22:35:04 +0100

    add post component part-2

--- components/Post.js
+++ components/Post.js
@@ -21,14 +21,19 @@ export default function Post({post}) {
                 {/* post text */}
                 <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">{post.text}</p>
                 {/* post image */}
-                <img src={post.img} alt="" />
+                <img className="rounded-2xl mr-2" src={post.img} alt="" />
                 {/* icons */}
-                <div>
-                    <ChatIcon className="h-9"/>
-                    <TrashIcon className="h-9"/>
-                    <HeartIcon className="h-9"/>
-                    <ShareIcon className="h-9"/>
-                    <ChartBarIcon className="h-9"/>
+                <div className="flex justify-between text-gray-500 p-2">
+                    <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100
+                    "/>
+                    <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100
+                    "/>
+                    <HeartIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100
+                    "/>
+                    <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100
+                    "/>
+                    <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100
+                    "/>
                 </div>
         </div>
     </div>
