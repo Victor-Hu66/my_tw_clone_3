6245cd6a9532c2171ecb8c23f486777fa53b8935
victor <victor.h@clarusway.com>
2023-11-01 22:34:29 +0100

    add article api for widgets section

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,6 +1,6 @@
 import { SearchIcon } from "@heroicons/react/outline";
 
-export default function Widgets() {
+export default function Widgets({newsResults}) {
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
         <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
@@ -9,6 +9,10 @@ export default function Widgets() {
                 <input className="absolute inset-0 rounded-full pl-11 border-gray-500 text-gray-700 focus:shadow-lg focus:bg-white bg-gray-100" type="text" placeholder="Search Twitter"/>
             </div>
         </div>
+
+        {newsResults.map((article, index) => (
+            <p key={index}>{article.title}</p>
+        ))}
     </div>
   )
 }
