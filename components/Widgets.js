be0a165f754ea4284551538e52ec867ddce1663d
victor <victor.h@clarusway.com>
2023-11-02 21:33:30 +0100

    add styling news widget section

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,4 +1,5 @@
 import { SearchIcon } from "@heroicons/react/outline";
+import News from "./News";
 
 export default function Widgets({newsResults}) {
   return (
@@ -9,10 +10,14 @@ export default function Widgets({newsResults}) {
                 <input className="absolute inset-0 rounded-full pl-11 border-gray-500 text-gray-700 focus:shadow-lg focus:bg-white bg-gray-100" type="text" placeholder="Search Twitter"/>
             </div>
         </div>
-
+        <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
+          <h4 className="font-bold text-xl px-4">What's happening</h4>
         {newsResults.map((article, index) => (
-            <p key={index}>{article.title}</p>
+            <News key={article.title} article={article}/>
         ))}
+        <button className="text-blue-300 pl-4 pb-3 hover:text-blue-400">Show more</button>
+        </div>
+
     </div>
   )
 }
