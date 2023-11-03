632d7193db4f6e9898c2bb987e00172819c77b3e
victor <victor.h@clarusway.com>
2023-11-03 16:30:39 +0100

    add fetched random user data as props

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Sidebar from "@/components/Sidebar";
 import Widgets from "@/components/Widgets";
 import Head from "next/head";
 
-export default function Home({newsResults}) {
+export default function Home({newsResults, randomUsersResults}) {
   return (
     <div>
       <Head>
@@ -18,22 +18,25 @@ export default function Home({newsResults}) {
         <Feed/>
 
         {/* Widgets */}
-        <Widgets newsResults={newsResults.articles}/>
+        <Widgets newsResults={newsResults.articles} randomUsersResults={randomUsersResults.results}/>
       </main>
     </div>
   );
 }
 
 
-// https://saurav.tech/NewsAPI/top-headlines/category/business/us.json
-
-
 export async function getServerSideProps(){
   const newsResults = await fetch("https://saurav.tech/NewsAPI/top-headlines/category/business/us.json")
   .then((res) => res.json())
+
+  // who to follow section
+  const randomUsersResults = await fetch("https://randomuser.me/api/?results=5&inc=name,login,picture")
+  .then((res) => res.json())
+ 
   return {
     props: {
       newsResults,
+      randomUsersResults
     }
   }
 }
