362fd29a63a8c9370c29e5a8c667ca7aec67f04c
victor <victor.h@clarusway.com>
2023-11-03 17:03:59 +0100

    add some styling to who to follow widget

--- components/Widgets.js
+++ components/Widgets.js
@@ -7,13 +7,17 @@ export default function Widgets({newsResults, randomUsersResults}) {
 
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5"> 
-        <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
+
+        {/* search bar */}
+        <div className="w-[80%] xl:w-[90%] sticky top-0 bg-white py-1.5 z-50">
             <div className="flex items-center p-3 rounded-full relative">
                 <SearchIcon className="h-5 z-50 text-gray-500"/>
                 <input className="absolute inset-0 rounded-full pl-11 border-gray-500 text-gray-700 focus:shadow-lg focus:bg-white bg-gray-100" type="text" placeholder="Search Twitter"/>
             </div>
         </div>
-        <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
+
+        {/* What's happening */}
+        <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[80%] xl:w-[90%]">
           <h4 className="font-bold text-xl px-4">What's happening</h4>
         {newsResults.slice(0,articleNum).map((article, index) => (
             <News key={article.title} article={article}/>
@@ -21,18 +25,22 @@ export default function Widgets({newsResults, randomUsersResults}) {
         <button onClick={() => setArticleNum(articleNum + 3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">Show more</button>
         </div>
 
-        <div className="text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[90%] xl:w-[75%]">
+        {/* Who to follow */}
+        <div className="text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[80%] xl:w-[90%]">
           <h4 className="font-bold text-xl px-4">Who to follow</h4>
           {randomUsersResults.map((randomUser) => (
-            <div key={randomUser.login.username}>
+            <div 
+            className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200"
+            key={randomUser.login.username}>
                  <img
                   className="rounded-full"
                   width="40"
                   src={randomUser.picture.thumbnail} alt="" />
-                  <div>
-                    <h4>{randomUser.login.username}</h4>
-                    <h5>{randomUser.name.first+" "+randomUser.name.last}</h5>
+                  <div className="truncate ml-4 leading-5">
+                    <h4 className="font-bold hover:underline text-[14px] truncate">{randomUser.login.username}</h4>
+                    <h5 className="text-[13px] truncate">{randomUser.name.first+" "+randomUser.name.last}</h5>
                   </div>
+                  <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold">Follow</button>
             </div>
           ))}
           <button className="text-blue-300 pl-4 pb-3 hover:text-blue-400">Show  more</button>
