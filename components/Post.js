0c4542516295abf6ed74b0ea941981c3817310d6
victor <victor.h@clarusway.com>
2024-01-08 23:22:32 +0100

    add like feature for posts

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,33 @@
+import { db } from "@/firebase";
 import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, ShareIcon, TrashIcon } from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import { collection, doc, onSnapshot, setDoc } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import Moment from "react-moment";
 
 export default function Post({post}) {
+
+  const { data: session} = useSession()
+  const [likes, setLikes] = useState([])
+  const [hasLiked, setHasLiked] = useState(false)
+ 
+  useEffect(() => { 
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", post.id, "likes"), (snapshot)=>setLikes(snapshot.docs)
+    )
+  }, [db])
+
+  useEffect(()=>{
+    setHasLiked(likes.findIndex((like) => like.id === session?.user?.uid) !== -1)
+  },[likes])
+
+  async function likePost(){ 
+    await setDoc(doc(db, "posts", post.id, "likes", session?.user?.uid), { 
+      username: session.user.username
+    })
+  } 
+
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200">
         {/* user image */}
@@ -29,8 +55,14 @@ export default function Post({post}) {
                     "/>
                     <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100
                     "/>
-                    <HeartIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100
+                    {hasLiked ? (
+                      <HeartIconFilled onClick={likePost} className="h-9 w-9 hoverEffect p-2  text-red-600 hover:bg-red-100
+                      "/>
+                    ) : (
+                      <HeartIcon onClick={likePost} className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100
                     "/>
+                    ) }
+                                         
                     <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100
                     "/>
                     <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100
