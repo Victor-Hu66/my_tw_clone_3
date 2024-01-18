db16d13f78fd467ba6d472d0627bfe8ba9bfbc9e
victor-h <victor.h@clarusway.com>
2024-01-18 15:43:43 +0100

    feat: Modal v2
    feat: Add comment modal v:2

--- atom/modalAtom.js
+++ atom/modalAtom.js
@@ -4,3 +4,7 @@ export  const modalState = atom({
     key: 'textState', // unique ID (with respect to other atoms/selectors)
     default: false  , // default value (aka initial value)
   });
+export  const postIdState = atom({
+    key: 'postIdState', // unique ID (with respect to other atoms/selectors)
+    default: "", // default value (aka initial value)
+  });
--- components/CommentModal.js
+++ components/CommentModal.js
@@ -1,9 +1,11 @@
 import { useRecoilState } from "recoil"
-import { modalState} from "../atom/modalAtom"
+import { modalState, postIdState} from "../atom/modalAtom"
 import Modal from "react-modal"
+import { XIcon } from "@heroicons/react/outline"
 
 export default function CommentModal() {
-    const [open, setOpen] = useRecoilState(modalState )
+    const [open, setOpen] = useRecoilState(modalState)
+    const [postId] = useRecoilState(postIdState) 
     return (
         <div>
             <h1>Comment Modal</h1>
@@ -11,8 +13,15 @@ export default function CommentModal() {
                 <Modal 
                 onRequestClose={() => setOpen(false)}
                 isOpen={open} 
-                className="max-w-lg w-[90%] absolute top-24 left-[50%] translate-x-[-50%] bg-white border-1 border-gray-200 rounded-xl shadow-md">
-                    <h1>Comment Modal</h1>
+                className="max-w-lg w-[90%] h-[300px] absolute top-24 left-[50%] translate-x-[-50%] bg-white border-1 border-gray-200 rounded-xl shadow-md">
+                    <div className="p-1">
+                        <div className="border-b border-gray-200 py-2 px-1.5">
+                            <div onClick={() => setOpen(false)} className="hoverEffect w-9 h-9 flex items-center justify-center">
+                                <XIcon className="h-[22px] text-gray-700"/>
+                            </div>
+                        </div>
+                        <h1>{postId}</h1>
+                    </div>
                 </Modal>
             )}
         </div>
